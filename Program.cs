90967205228ec22b3aafbee5d48cb74e60e2492a
David Arroyo <29355377+davidarroyo1234@users.noreply.github.com>
2023-08-26 18:57:55 +0200

    Final version 1.2.0

--- Program.cs
+++ Program.cs
@@ -1,28 +1,28 @@
 ﻿using System;
 using System.Drawing;
-using System.Media;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using AutoAccept_CSGO4.HideChecker;
-using AutoAccept_CSGO4.AntiAFK;
-
+using AutoAccept_CSGO4.TelegramBot;
 
 namespace AutoAccept_CSGO4
 {
     internal class Program
     {
+        private const uint LEFTMOUSE_CLICKDOWN = 0x0002;
+        private const uint LEFTMOUSE_CLICKUP = 0x0004;
+
+        private TelegramBotHandler telegramBotHandler;
+
         [DllImport("user32.dll")]
         private static extern void mouse_event(uint dwflags, uint dx, uint dy, uint dwData, uint dwExtraInf);
 
         [DllImport("user32.dll")]
         public static extern bool SetCursorPos(int x, int y);
 
-
-        private const uint LEFTMOUSE_CLICKDOWN = 0x0002;
-        private const uint LEFTMOUSE_CLICKUP = 0x0004;
-
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.WriteLine(@"
  _____  _____ _____ _____         ___        _         ___                     _                             
@@ -42,45 +42,117 @@ ______                       _             _     _                              
         __/ |     \____/                                                 __/ |                               
        |___/                                                            |___/                                
 ");
-            new Program().Init(args);
+            await new Program().Init(args);
         }
 
-        private void Init(string[] args)
+        private async Task Init(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    await StartDecision();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error has occurred Restarting..." + e.Message);
+                }
+            }
+        }
+
+        private async Task StartDecision()
+        {
+            Console.WriteLine(
+                "1- Start bot with Telegram notifications\n" +
+                "2- Start bot without Telegram notifications\n" +
+                (TelegramBotHandler.HasTelegramToken() ? "3- Delete telegram token\n" : "")
+            );
+
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    await HandleTelegramTokenBeforeInitialization();
+                    break;
+                case "2":
+                    Console.WriteLine("Starting bot without Telegram notifications...");
+                    await BeginSearch();
+                    break;
+                case "3" when TelegramBotHandler.HasTelegramToken():
+                    TelegramBotHandler.DeleteTelegramToken();
+                    Console.WriteLine("Token deleted");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            await StartDecision();
+        }
+
+        private async Task HandleTelegramTokenBeforeInitialization()
+        {
+            telegramBotHandler = new TelegramBotHandler();
+
+            if (TelegramBotHandler.HasTelegramToken())
+            {
+                await telegramBotHandler.InitializeTelegramBot();
+                Console.WriteLine("Starting bot with Telegram notifications...");
+            }
+            else
+            {
+                Console.WriteLine("Enter your telegram bot token:");
+                var token = Console.ReadLine();
+                if (!String.IsNullOrEmpty(token))
+                {
+                    TelegramBotHandler.SetTelegramToken(token);
+                    await telegramBotHandler.InitializeTelegramBot();
+                    Console.WriteLine("Starting bot with Telegram notifications...");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid token");
+                    return;
+                }
+            }
+
+            await BeginSearch();
+        }
+
+        private async Task BeginSearch()
         {
             while (true)
             {
                 Thread.Sleep(100);
 
-                var csgOopenChecker = new CsgOopenChecker();
+                var csgoOpenChecker = new CsgoOpenChecker();
 
-                if (csgOopenChecker.MinimizedCheck() == -1)
+                if (csgoOpenChecker.MinimizedCheck() == -1)
                 {
                     Console.WriteLine("Open csgo to start");
                 }
-                else if (csgOopenChecker.MinimizedCheck() == 1)
+                else if (csgoOpenChecker.MinimizedCheck() == 1)
                 {
                     Console.WriteLine("Maximize CSGO");
                 }
 
-                while (csgOopenChecker.MinimizedCheck() != 0)
+                while (csgoOpenChecker.MinimizedCheck() != 0)
                 {
                     Thread.Sleep(300);
                 }
 
-                SearchPixel();
+                await SearchPixel();
                 Thread.Sleep(100);
             }
         }
 
-
-        //Starts searching for the accept button in the screen.
-        private void SearchPixel()
+        private async Task SearchPixel()
         {
             var counter = 0;
             var exitLoop = false;
             Console.WriteLine("Searching button...");
 
-            //Accept button color declaration.
             var color = Color.FromArgb(76, 175, 80);
             var color2 = Color.FromArgb(90, 203, 94);
 
@@ -88,16 +160,11 @@ ______                       _             _     _                              
             var graphics = Graphics.FromImage(bitmap);
             graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
 
-
-            //Checks the pixels from x:0 and y:0 to your screen resolution.
             for (var x = 0; x < bitmap.Width && !exitLoop; x++)
             {
                 for (var y = 0; y < bitmap.Height; y++)
                 {
                     var searchPixel = bitmap.GetPixel(x, y);
-                    //If the current checking color matches the declared colors it will add +1 to the counter.
-                    //This counter is used to prevent to click any other matching color that is not the accept button.
-                    //The csgo accept button contains more than 9000 green pixels.
 
                     if (searchPixel == color || searchPixel == color2)
                     {
@@ -105,24 +172,40 @@ ______                       _             _     _                              
                         {
                             Thread.Sleep(200);
                             SetCursorPos(x, y);
-                            Console.WriteLine("Button found! Pressing...");
-                            ClickF(x, y);
+                            await ClickFunc(x, y);
                             exitLoop = true;
                             break;
                         }
+
                         counter++;
                     }
                 }
             }
         }
 
-        private void ClickF(int x, int y)
+        private async Task ClickFunc(int x, int y)
         {
             Console.WriteLine("Clicking...");
             SetCursorPos(x, y);
             mouse_event(LEFTMOUSE_CLICKDOWN, 0, 0, 0, 0);
             mouse_event(LEFTMOUSE_CLICKUP, 0, 0, 0, 0);
+            
+            if (telegramBotHandler != null && telegramBotHandler.isInitialized)
+            {
+                try
+                {
+                    Console.WriteLine("Sending notification...");
+                    await telegramBotHandler.SendMatchFoundToAllClients();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error sending notification: " + e.Message);
+                }
+            }
+            
             Thread.Sleep(200);
+
+
         }
     }
 }
